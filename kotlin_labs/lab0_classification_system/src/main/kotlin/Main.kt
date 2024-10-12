fc8345677ba47eefe617f436cc9799036cbd8430
Andre Chicu <andriesh.chicu@gmail.com>
2024-10-12 22:51:05 +0300

    define creature class and types

--- kotlin_labs/lab0_classification_system/src/main/kotlin/Main.kt
+++ kotlin_labs/lab0_classification_system/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package main
 
+import kotlin.io.println
+
 class Greeter() {
   fun greet(greeting: String, name: String, planet: Planet?) {
     println(greeting + ", " + name + "!")
@@ -21,7 +23,51 @@ fun String.toPlanet(): Planet? {
   return Planet.values().find { it.name.equals(this, ignoreCase = true) }
 }
 
+enum class Trait(displayName: String) {
+  HAIRY("HAIRY"),
+  TALL("TAIL"),
+  SHORT("SHORT"),
+  BLONDE("BLONDE"),
+  EXTRA_ARMS("EXTRA_ARMS"),
+  EXTRA_HEAD("EXTRA_HEAD"),
+  GREEN("GREEN"),
+  BULKY("BULKY"),
+  POINTY_EARS("POINTY_EARS");
+  val displayName = displayName
+}
+
+enum class Race(displayName: String) {
+  ELF("Elf"),
+  WOOKIE("Wookie"),
+  EWOK("Ewok"),
+  ASGARDIAN("Asgardian"),
+  BETELGEUSIAN("Betelgeusian"),
+  VOGONS("Vogons"),
+  DWARF("Dwarf");
+  val displayName = displayName
+}
+
+class Creature(
+        val isHumanoid: Boolean?,
+        val planet: Planet?,
+        val age: Int?,
+        val traits: List<Trait>?
+) {
+  var race: Race? = null
+
+  fun calcRace() {}
+}
+
 fun main() {
-  val planet = "Earth".toPlanet()
-  Greeter().greet("Hello there", "Obiwan", planet)
+  val elf =
+          Creature(
+                  isHumanoid = true,
+                  planet = Planet.EARTH,
+                  age = 2000,
+                  traits = listOf(Trait.SHORT)
+          )
+  println("isHumanoid: ${elf.isHumanoid}")
+  println("planet: ${elf.planet}")
+  println("age: ${elf.age}")
+  elf.traits?.forEach { trait -> println("Trait: ${trait.name}") }
 }
--- kotlin_labs/settings.gradle.kts
+++ kotlin_labs/settings.gradle.kts
@@ -5,9 +5,7 @@
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.10.2/userguide/multi_project_builds.html in the Gradle documentation.
  */
 
-plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0" }
 
 rootProject.name = "kotlin-labs"
 
