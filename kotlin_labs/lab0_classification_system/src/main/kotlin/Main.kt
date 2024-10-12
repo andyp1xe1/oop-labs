8d2869bdef9de0cbf0fc180190eb80145b7cd776
Andre Chicu <andriesh.chicu@gmail.com>
2024-10-12 12:32:39 +0300

    add planet enum

--- kotlin_labs/lab0_classification_system/src/main/kotlin/Main.kt
+++ kotlin_labs/lab0_classification_system/src/main/kotlin/Main.kt
@@ -1,11 +1,27 @@
 package main
 
 class Greeter() {
-  fun greet(greeting: String, name: String) {
-    println(greeting +  ", " + name + "!")
+  fun greet(greeting: String, name: String, planet: Planet?) {
+    println(greeting + ", " + name + "!")
+    println("are you from ${planet?.displayName}?")
   }
 }
 
+enum class Planet(displayName: String) {
+  EARTH("Earth"),
+  ASGARD("Asgard"),
+  BETELGEUSE("Betelgeuse"),
+  VOGSPHERE("Vogsphere"),
+  KASHYYYK("Kashyyyk"),
+  ENDOR("Endor");
+  val displayName = displayName
+}
+
+fun String.toPlanet(): Planet? {
+  return Planet.values().find { it.name.equals(this, ignoreCase = true) }
+}
+
 fun main() {
-  Greeter().greet("Hello there", "Obiwan")
+  val planet = "Earth".toPlanet()
+  Greeter().greet("Hello there", "Obiwan", planet)
 }
