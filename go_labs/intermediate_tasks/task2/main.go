d39f07c170cf716b0dede3acc0bd3346e128df1b
Andre Chicu <andriesh.chicu@gmail.com>
2024-10-29 22:08:04 +0200

    golang -- task 2

---
+++ go_labs/intermediate_tasks/task2/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/andyp1xe1/oop-labs/go_labs/intermediate_tasks/task2/textdata"
+	"os"
+)
+
+var fileReader = FileReader{}
+
+type FileReader struct{}
+
+func (f FileReader) readFileIntoString(path string) (string, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide the path to the .txt file.")
+		return
+	}
+
+	filePath := os.Args[1]
+
+	text, err := fileReader.readFileIntoString(filePath)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	textData := textdata.NewTextData(filePath, text)
+
+	fmt.Printf("File Name: %v\n", textData.GetFileName())
+	fmt.Printf("Text Content: %v\n", textData.GetText())
+	fmt.Printf("Number of Vowels: %v\n", textData.GetNumberOfVowels())
+	fmt.Printf("Number of Consonants: %v\n", textData.GetNumberOfConsonants())
+	fmt.Printf("Number of Letters: %v\n", textData.GetNumberOfLetters())
+	fmt.Printf("Number of Sentences: %v\n", textData.GetNumberOfSentences())
+	fmt.Printf("Longest Word: %v\n", textData.GetLongestWord())
+}
